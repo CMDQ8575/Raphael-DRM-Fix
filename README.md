e3d5bfffd0dd4a5d354572b61e48387f2b237404
ZhuHao <zhuhao@wisherai.com>
2023-08-28 12:25:43 +0800

    Inital Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Raphael DRM Fix
+
+A magisk module for Raphael (Redmi K20 Pro) to add Widevine L1 support.
+
+## Support Devices
+
+Raphael (Redmi K20 Pro)
+
+## Step
+
+1. Flash this module in magisk.
+2. Update Certificate on Fingerprint payments - Widevine(L1).
+3. Check your Widevine Level by DRM Info
---
+++ customize.sh
@@ -0,0 +1,16 @@
+if [ "$(getprop ro.product.system.device)" != "raphael" ]; then
+    abort "This module is for Redmi K20 Pro only"
+fi
+array=$(find /system/etc /vendor/etc /product/etc -name raphael.xml)
+for CFG in $array
+do
+    [[ -f $CFG ]] && [[ ! -L $CFG ]] && {
+    SELECTPATH=$CFG
+    mkdir -p `dirname $MODPATH$CFG`
+    ui_print "- Modifying $SELECTPATH"
+    cp -af $SELECTPATH $MODPATH/system$SELECTPATH
+    ui_print "- Starting modifiy"
+    sed -i 's|<integer name="support_widevine_l1">1</integer>|<integer name="support_widevine_l1">2</integer>|g' $MODPATH/system$SELECTPATH
+    }
+done
+set_perm_recursive "$MODPATH/system/vendor/lib64" 0 0 0755 0644 u:object_r:vendor_file:s0
